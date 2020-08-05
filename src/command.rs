bef8b65f139ea12ac9bd1a7d464ecfe9bf9644a6
Clément Nerma <clement.nerma@gmail.com>
2020-08-05 17:32:31 +0200

    Document all command-line options

--- src/command.rs
+++ src/command.rs
@@ -41,52 +41,52 @@ pub enum Action {
 
 #[derive(Clap)]
 pub struct ListTrashItems {
-    #[clap(long)]
+    #[clap(long, about = "Only list occurrences of items with a specific name")]
     pub name: Option<String>,
 
-    #[clap(short, long)]
+    #[clap(short, long, about = "Show details (size, number of files and directories)")]
     pub details: bool
 }
 
 #[derive(Clap)]
 pub struct MoveToTrash {
-    #[clap()]
+    #[clap(about = "Item path to move to the trash")]
     pub path: String,
 
-    #[clap(short, long)]
+    #[clap(short, long, about = "Delete the item permanently")]
     pub permanently: bool,
 
-    #[clap(short, long, conflicts_with="permanently")]
+    #[clap(short, long, conflicts_with="permanently", about = "For external filesystems, move the item to the main filesystem's trash directory")]
     pub move_ext_filesystems: bool,
 
-    #[clap(short, long, conflicts_with="permanently")]
+    #[clap(short, long, conflicts_with="permanently", about = "Do not fail when encoutering invalid UTF-8 file names")]
     pub allow_invalid_utf8_item_names: bool,
 }
 
 #[derive(Clap)]
 pub struct RestoreItem {
-    #[clap()]
+    #[clap(about = "Name of the item to restore")]
     pub filename: String,
 
-    #[clap(long, parse(from_os_str))]
+    #[clap(long, parse(from_os_str), about = "Destination path (defaults to the current directory)")]
     pub to: Option<PathBuf>,
 
-    #[clap(long)]
+    #[clap(long, about = "ID of the item to restore in case multiple exist with the same name")]
     pub id: Option<String>,
 
-    #[clap(short, long, conflicts_with="permanently")]
+    #[clap(short, long, conflicts_with="permanently", about = "For external filesystems, move the item from the main filesystem's trash directory")]
     pub move_ext_filesystems: bool,
 
-    #[clap(short, long)]
+    #[clap(short, long, about = "Overwrite target path if it already exists")]
     pub force: bool
 }
 
 #[derive(Clap)]
 pub struct DropItem {
-    #[clap()]
+    #[clap(about = "Name of the item to permanently delete from the trash")]
     pub filename: String,
 
-    #[clap(long)]
+    #[clap(long, about = "ID of the item to drop in case multiple exist with the same name")]
     pub id: Option<String>
 }
 
