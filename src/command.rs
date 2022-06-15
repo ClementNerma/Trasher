cb50b94761413b2219e6ce1b3f1b936ed7ac5423
Clément Nerma <clement.nerma@gmail.com>
2022-06-15 19:01:17 +0200

    Update dependencies

--- src/command.rs
+++ src/command.rs
@@ -1,12 +1,12 @@
-use clap::{crate_version, Clap};
+use clap::{Args, Parser, Subcommand};
 use std::path::PathBuf;
 
 lazy_static! {
     pub static ref OPTS: Opts = Opts::parse();
 }
 
-#[derive(Clap)]
-#[clap(version = crate_version!(), author = "Clément Nerma <clement.nerma@gmail.com>")]
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
 pub struct Opts {
     #[clap(short, long, parse(from_os_str))]
     pub trash_dir: PathBuf,
@@ -14,7 +14,7 @@ pub struct Opts {
     #[clap(short, long)]
     pub create_trash_dir: bool,
 
-    #[clap(long, about = "Don't clean up the transfer directory automatically")]
+    #[clap(long, help = "Don't clean up the transfer directory automatically")]
     pub no_cleanup: bool,
 
     #[clap(short, long)]
@@ -24,143 +24,143 @@ pub struct Opts {
     pub action: Action,
 }
 
-#[derive(Clap)]
+#[derive(Subcommand)]
 pub enum Action {
-    #[clap(name = "ls", about = "List all items in the trash")]
+    #[clap(name = "ls", help = "List all items in the trash")]
     List(ListTrashItems),
 
-    #[clap(name = "rm", about = "Move an item to the trash")]
+    #[clap(name = "rm", help = "Move an item to the trash")]
     Remove(MoveToTrash),
 
     #[clap(
         name = "unrm",
         alias = "restore",
-        about = "Restore an item from the trash"
+        help = "Restore an item from the trash"
     )]
     Restore(RestoreItem),
 
-    #[clap(name = "drop", about = "Permanently delete an item from the trash")]
+    #[clap(name = "drop", help = "Permanently delete an item from the trash")]
     Drop(DropItem),
 
     #[clap(
         name = "path-of",
-        about = "Get the path of an item inside the trash directory"
+        help = "Get the path of an item inside the trash directory"
     )]
     PathOf(GetItemPath),
 
-    #[clap(name = "clear", about = "Permanently delete all items in the trash")]
+    #[clap(name = "clear", help = "Permanently delete all items in the trash")]
     Clear(EmptyTrash),
 }
 
-#[derive(Clap)]
+#[derive(Args)]
 pub struct ListTrashItems {
-    #[clap(long, about = "Only list occurrences of items with a specific name")]
+    #[clap(long, help = "Only list occurrences of items with a specific name")]
     pub name: Option<String>,
 
     #[clap(
         short,
         long,
-        about = "Show details (size, number of files and directories)"
+        help = "Show details (size, number of files and directories)"
     )]
     pub details: bool,
 }
 
-#[derive(Clap)]
+#[derive(Args)]
 pub struct MoveToTrash {
-    #[clap(about = "Path of the items to move to the trash")]
+    #[clap(help = "Path of the items to move to the trash")]
     pub paths: Vec<String>,
 
-    #[clap(short, long, about = "Delete the items permanently")]
+    #[clap(short, long, help = "Delete the items permanently")]
     pub permanently: bool,
 
     #[clap(
         short,
         long,
-        about = "Do nothing if the item doesn't exist instead of failing"
+        help = "Do nothing if the item doesn't exist instead of failing"
     )]
     pub ignore: bool,
 
     #[clap(
         short,
         long,
-        about = "For external filesystems, move the items to the main filesystem's trash directory"
+        help = "For external filesystems, move the items to the main filesystem's trash directory"
     )]
     pub move_ext_filesystems: bool,
 
     #[clap(
         short,
         long,
-        about = "Only apply '--move-ext-filesystems' if the items' size is lower or equal to the provided one"
+        help = "Only apply '--move-ext-filesystems' if the items' size is lower or equal to the provided one"
     )]
     pub size_limit_move_ext_filesystems: Option<String>,
 
     #[clap(
         short,
         long,
-        about = "Do not fail when encoutering invalid UTF-8 file names"
+        help = "Do not fail when encoutering invalid UTF-8 file names"
     )]
     pub allow_invalid_utf8_item_names: bool,
 }
 
-#[derive(Clap)]
+#[derive(Args)]
 pub struct RestoreItem {
-    #[clap(about = "Name of the item to restore")]
+    #[clap(help = "Name of the item to restore")]
     pub filename: String,
 
     #[clap(
         long,
         parse(from_os_str),
-        about = "Destination path (defaults to the current directory)"
+        help = "Destination path (defaults to the current directory)"
     )]
     pub to: Option<PathBuf>,
 
     #[clap(
         long,
-        about = "ID of the item to restore in case multiple exist with the same name"
+        help = "ID of the item to restore in case multiple exist with the same name"
     )]
     pub id: Option<String>,
 
     #[clap(
         short,
         long,
-        about = "For external filesystems, move the item from the main filesystem's trash directory"
+        help = "For external filesystems, move the item from the main filesystem's trash directory"
     )]
     pub move_ext_filesystems: bool,
 
-    #[clap(short, long, about = "Overwrite target path if it already exists")]
+    #[clap(short, long, help = "Overwrite target path if it already exists")]
     pub force: bool,
 }
 
-#[derive(Clap)]
+#[derive(Args)]
 pub struct DropItem {
-    #[clap(about = "Name of the item to permanently delete from the trash")]
+    #[clap(help = "Name of the item to permanently delete from the trash")]
     pub filename: String,
 
     #[clap(
         long,
-        about = "ID of the item to drop in case multiple exist with the same name"
+        help = "ID of the item to drop in case multiple exist with the same name"
     )]
     pub id: Option<String>,
 }
 
-#[derive(Clap)]
+#[derive(Args)]
 pub struct GetItemPath {
-    #[clap(about = "Name of the item to get the path of in the trash")]
+    #[clap(help = "Name of the item to get the path of in the trash")]
     pub filename: String,
 
     #[clap(
         long,
-        about = "ID of the item to get in case multiple exist with the same name"
+        help = "ID of the item to get in case multiple exist with the same name"
     )]
     pub id: Option<String>,
 
     #[clap(
         short,
         long,
-        about = "Do not fail if the path contains invalid UTF-8 characters"
+        help = "Do not fail if the path contains invalid UTF-8 characters"
     )]
     pub allow_invalid_utf8_path: bool,
 }
 
-#[derive(Clap)]
+#[derive(Args)]
 pub struct EmptyTrash {}
