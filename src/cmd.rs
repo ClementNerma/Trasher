35d5e7c9e8461af875d362055ea2b2770d1fb9a5
Clément Nerma <clement.nerma@gmail.com>
2025-02-02 16:13:42 +0100

    Simplify modules

---
+++ src/cmd.rs
@@ -0,0 +1,127 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+use log::LevelFilter;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+pub struct Cmd {
+    #[clap(
+        short,
+        long,
+        global = true,
+        help = "Level of verbosity",
+        default_value = "info"
+    )]
+    pub verbosity: LevelFilter,
+
+    #[clap(
+        global = true,
+        short,
+        long,
+        help = "Disallow making a filesystem-local trash directory in some paths"
+    )]
+    pub exclude: Vec<PathBuf>,
+
+    #[clap(subcommand)]
+    pub action: Action,
+}
+
+#[derive(Subcommand)]
+pub enum Action {
+    #[clap(name = "ls", about = "List all items in the trash")]
+    List {
+        #[clap(long, help = "Only list occurrences of items with a specific name")]
+        name: Option<String>,
+    },
+
+    #[clap(name = "rm", about = "Move an item to the trash")]
+    Remove {
+        #[clap(num_args = 1.., help = "Path of the items to move to the trash")]
+        paths: Vec<String>,
+
+        #[clap(short, long, help = "Delete the items permanently")]
+        permanently: bool,
+
+        #[clap(
+            short,
+            long,
+            help = "Do nothing if the item doesn't exist instead of failing"
+        )]
+        ignore: bool,
+
+        #[clap(
+            short,
+            long,
+            help = "Do not fail when encoutering invalid UTF-8 file names"
+        )]
+        allow_invalid_utf8_item_names: bool,
+    },
+
+    #[clap(
+        name = "unrm",
+        alias = "restore",
+        about = "Restore an item from the trash"
+    )]
+    Restore {
+        #[clap(help = "Name of the item to restore")]
+        filename: Option<String>,
+
+        #[clap(
+            long,
+            help = "Destination path (defaults to the current directory)",
+            requires = "filename"
+        )]
+        to: Option<PathBuf>,
+
+        #[clap(
+            long,
+            help = "ID of the item to restore in case multiple exist with the same name",
+            requires = "filename"
+        )]
+        id: Option<String>,
+    },
+
+    #[clap(name = "drop", about = "Permanently delete an item from the trash")]
+    Drop {
+        #[clap(help = "Name of the item to permanently delete from the trash")]
+        filename: String,
+
+        #[clap(
+            long,
+            help = "ID of the item to drop in case multiple exist with the same name"
+        )]
+        id: Option<String>,
+    },
+
+    #[clap(
+        name = "path-of",
+        about = "Get the path of an item inside the trash directory"
+    )]
+    PathOf {
+        #[clap(help = "Name of the item to get the path of in the trash")]
+        filename: String,
+
+        #[clap(
+            long,
+            help = "ID of the item to get in case multiple exist with the same name"
+        )]
+        id: Option<String>,
+
+        #[clap(
+            short,
+            long,
+            help = "Do not fail if the path contains invalid UTF-8 characters"
+        )]
+        allow_invalid_utf8_path: bool,
+    },
+
+    #[clap(
+        name = "trash-path",
+        about = "Get the path of the trash directory for the current folder"
+    )]
+    TrashPath,
+
+    #[clap(name = "empty", about = "Permanently delete all items in the trash")]
+    Empty,
+}
--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,38 @@
 // NOTE: NIGHTLY
 #![cfg_attr(target_family = "windows", feature(windows_by_handle))]
 
-mod actions;
-mod args;
+mod cmd;
 mod fsutils;
 mod fuzzy;
 mod items;
 mod logger;
 
-use std::{path::PathBuf, process::ExitCode};
+use std::{collections::BTreeSet, fs, io::stdin, path::PathBuf, process::ExitCode};
 
-use anyhow::{bail, Result};
-use args::*;
+use anyhow::{bail, Context, Result};
 use clap::Parser;
-use log::error;
+use cmd::*;
+use indicatif::{ProgressBar, ProgressStyle};
+use jiff::Zoned;
+use log::{debug, error, info, warn};
 
-use self::{fsutils::compute_exclusions, logger::Logger};
+use self::{
+    fsutils::{
+        are_on_same_fs, compute_exclusions, determine_trash_dir_for, expect_single_trash_item,
+        is_dangerous_path, list_all_trash_items, list_deletable_fs_items, move_item_pbr,
+        table_for_items, TrashedItem, TRASH_TRANSFER_DIRNAME,
+    },
+    fuzzy::{run_fuzzy_finder, FuzzyFinderItem},
+    items::TrashItemInfos,
+    logger::Logger,
+};
 
 fn main() -> ExitCode {
-    let Args {
+    let Cmd {
         verbosity,
         exclude,
         action,
-    } = Args::parse();
+    } = Cmd::parse();
 
     // Set up the logger
     Logger::new(verbosity).init().unwrap();
@@ -46,13 +56,358 @@ fn inner_main(action: Action, exclude: &[PathBuf]) -> Result<()> {
     let exclude_dirs = compute_exclusions(exclude)?;
 
     match action {
-        Action::List(args) => actions::list(args, &exclude_dirs)?,
-        Action::Remove(args) => actions::remove(args, &exclude_dirs)?,
-        Action::Drop(args) => actions::drop(args, &exclude_dirs)?,
-        Action::PathOf(args) => actions::path_of(args, &exclude_dirs)?,
-        Action::Restore(args) => actions::restore(args, &exclude_dirs)?,
-        Action::Empty => actions::empty(&exclude_dirs)?,
-        Action::TrashPath => actions::trash_path(&exclude_dirs)?,
+        Action::List { name } => {
+            debug!("Listing trash items...");
+
+            let mut items = list_all_trash_items(&exclude_dirs)?;
+
+            if items.is_empty() {
+                info!("All trashes are empty.");
+                return Ok(());
+            }
+
+            if let Some(name) = &name {
+                debug!("Filtering {} items by name...", items.len());
+                items.retain(|trashed| trashed.data.filename.contains(name));
+
+                if items.is_empty() {
+                    info!("No item in trash match the provided name.");
+                    return Ok(());
+                }
+            }
+
+            println!("{}", table_for_items(&items));
+        }
+
+        Action::Remove {
+            paths,
+            permanently,
+            ignore,
+            allow_invalid_utf8_item_names,
+        } => {
+            debug!("Going to remove {} item(s)...", paths.len());
+
+            for (i, path) in paths.iter().enumerate() {
+                debug!("Treating item {} on {}...", i + 1, paths.len());
+
+                let path = PathBuf::from(path);
+
+                debug!("Checking if item exists...");
+
+                if is_dangerous_path(&path) {
+                    bail!("Removing this path is too dangerous, operation aborted.");
+                }
+
+                if !path.exists() {
+                    if ignore {
+                        continue;
+                    }
+
+                    bail!("No item exists at path: {}", path.display());
+                }
+
+                if permanently {
+                    let deletion_result = if path.is_file() {
+                        fs::remove_file(&path)
+                    } else {
+                        fs::remove_dir_all(&path)
+                    };
+
+                    match deletion_result {
+                        Err(err) => bail!("Failed to permanently remove item: {}", err),
+                        Ok(()) => continue,
+                    }
+                }
+
+                let filename = path
+                    .file_name()
+                    .context("Specified item path has no file name")?;
+
+                let filename = match filename.to_str() {
+                    Some(str) => str.to_string(),
+                    None => {
+                        if allow_invalid_utf8_item_names {
+                            filename.to_string_lossy().to_string()
+                        } else {
+                            bail!("Specified item does not have a valid UTF-8 file name")
+                        }
+                    }
+                };
+
+                let data = TrashItemInfos::new_now(filename.to_string());
+
+                debug!(
+                    "Moving item to trash under name '{}'...",
+                    data.trash_filename()
+                );
+
+                let trash_dir =
+                    determine_trash_dir_for(&path, &exclude_dirs).with_context(|| {
+                        format!(
+                            "Failed to determine path to the trash directory for item: {}",
+                            path.display()
+                        )
+                    })?;
+
+                if !trash_dir.exists() {
+                    fs::create_dir(&trash_dir).with_context(|| {
+                        format!(
+                            "Failed to create trash directory at path '{}'",
+                            trash_dir.display()
+                        )
+                    })?;
+                }
+
+                let trash_transfer_dir = trash_dir.join(TRASH_TRANSFER_DIRNAME);
+
+                if !trash_transfer_dir.exists() {
+                    fs::create_dir(&trash_transfer_dir).with_context(|| {
+                        format!(
+                            "Failed to create trash's partial transfer directory at path '{}'",
+                            trash_transfer_dir.display()
+                        )
+                    })?;
+                }
+
+                if !are_on_same_fs(&path, &trash_dir)? {
+                    info!("Moving item to trash directory {}", trash_dir.display());
+
+                    let transfer_path = trash_transfer_dir.join(data.trash_filename());
+
+                    move_item_pbr(&path, &transfer_path)
+                        .context("Failed to move item to the trash")?;
+
+                    fs::rename(&transfer_path, trash_dir.join(data.trash_filename()))
+                        .context("Failed to move item to the final trash directory")?;
+                } else {
+                    let trash_item = TrashedItem { data, trash_dir };
+                    let trash_item_path = trash_item.transfer_trash_item_path();
+
+                    fs::rename(&path, &trash_item_path).with_context(|| {
+                        format!("Failed to move item '{}' to trash", path.display())
+                    })?;
+
+                    fs::rename(&trash_item_path, trash_item.complete_trash_item_path())
+                        .with_context(|| {
+                            format!(
+                                "Failed to move fully transferred item '{}' to trash",
+                                path.display()
+                            )
+                        })?;
+                }
+            }
+        }
+
+        Action::Drop { filename, id } => {
+            debug!("Listing trash items...");
+
+            let item = expect_single_trash_item(&filename, id.as_deref(), &exclude_dirs)?;
+
+            debug!("Permanently removing item from trash...");
+
+            let path = item.complete_trash_item_path();
+
+            let result = if path.is_dir() {
+                fs::remove_dir_all(path)
+            } else {
+                fs::remove_file(path)
+            };
+
+            result.with_context(|| {
+                format!("Failed to remove item '{}' from trash", item.data.filename)
+            })?;
+        }
+
+        Action::PathOf {
+            filename,
+            id,
+            allow_invalid_utf8_path,
+        } => {
+            debug!("Listing trash items...");
+
+            let item = expect_single_trash_item(&filename, id.as_deref(), &exclude_dirs)?;
+            let item_path = item.complete_trash_item_path();
+
+            match item_path.to_str() {
+                Some(path) => println!("{}", path),
+                None => {
+                    if allow_invalid_utf8_path {
+                        println!("{}", item_path.to_string_lossy())
+                    } else {
+                        bail!(
+                            "Path contains invalid UTF-8 characters (lossy: {})",
+                            item_path.to_string_lossy()
+                        );
+                    }
+                }
+            }
+        }
+
+        Action::Restore { filename, to, id } => {
+            debug!("Listing trash items...");
+
+            let item = match filename {
+                Some(filename) => {
+                    expect_single_trash_item(&filename, id.as_deref(), &exclude_dirs)?
+                }
+
+                None => {
+                    let items = list_all_trash_items(&exclude_dirs)?;
+
+                    if items.is_empty() {
+                        info!("Trash is empty");
+                        return Ok(());
+                    }
+
+                    run_fuzzy_finder(
+                        items
+                            .into_iter()
+                            .map(|item| FuzzyFinderItem {
+                                display: format!(
+                                    "[{}] {}",
+                                    Zoned::try_from(item.data.datetime)
+                                        .and_then(|date| jiff::fmt::rfc2822::to_string(&date))
+                                        .unwrap_or_else(|_| "<Failed to format date>".to_owned()),
+                                    item.data.filename
+                                ),
+                                value: item,
+                            })
+                            .collect(),
+                    )?
+                }
+            };
+
+            let item_path = item.complete_trash_item_path();
+
+            let target_path = match to {
+                Some(to) => to,
+                None => std::env::current_dir().context("Failed to get current directory")?,
+            };
+
+            let target_path = target_path.join(&item.data.filename);
+
+            if target_path.exists() {
+                bail!("Target path already exists.");
+            }
+
+            let target_parent = target_path.parent().unwrap();
+
+            if !target_parent.exists() {
+                bail!(
+                    "Target directory '{}' does not exist",
+                    target_parent.display()
+                );
+            }
+
+            let result = if are_on_same_fs(&item.complete_trash_item_path(), target_parent)? {
+                debug!("Restoring item from trash...");
+
+                fs::rename(item_path, &target_path).context("Rename operation failed")
+            } else {
+                info!("Moving file across filesystems...");
+
+                move_item_pbr(&item_path, &target_path)
+            };
+
+            result.with_context(|| {
+                format!("Failed to restore item '{}' from trash", item.data.filename)
+            })?;
+        }
+
+        Action::Empty => {
+            let items = list_all_trash_items(&exclude_dirs)?;
+
+            if items.is_empty() {
+                info!("Trash is empty");
+                return Ok(());
+            }
+
+            let trash_dirs = items
+                .iter()
+                .map(|item| &item.trash_dir)
+                .collect::<BTreeSet<_>>();
+
+            warn!("You are about to delete the entire trash directories of:\n");
+
+            for trash_dir in &trash_dirs {
+                warn!(
+                    "  {} ({} items)",
+                    trash_dir.display(),
+                    items
+                        .iter()
+                        .filter(|item| &&item.trash_dir == trash_dir)
+                        .count()
+                );
+            }
+
+            warn!("\nAre you sure you want to continue [y/N]?");
+
+            let mut confirm_str = String::new();
+
+            stdin()
+                .read_line(&mut confirm_str)
+                .context("Failed to get user confirmation")?;
+
+            if !confirm_str.trim().eq_ignore_ascii_case("y") {
+                warn!("Cancelled.");
+                return Ok(());
+            }
+
+            info!("Emptying the trash...");
+
+            for trash_dir in trash_dirs {
+                info!("Emptying trash directory: {}", trash_dir.display());
+
+                warn!("> Listing files and directories to delete...");
+
+                let items = list_deletable_fs_items(trash_dir)?;
+
+                warn!("> Deleting {} item(s) recursively...", items.len());
+
+                let pbr = ProgressBar::new(items.len().try_into().unwrap());
+
+                pbr.set_style(ProgressStyle::default_bar()
+                    .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {human_pos}/{human_len} ({eta})")
+                    .expect("Invalid progress bar template")
+                    .progress_chars("#>-"));
+
+                for (i, item) in items.iter().enumerate() {
+                    let metadata = item
+                        .symlink_metadata()
+                        .with_context(|| {
+                            format!("Failed to get metadata for item: {}", item.display())
+                        })?
+                        .file_type();
+
+                    if metadata.is_dir() {
+                        fs::remove_dir(item).with_context(|| {
+                            format!("Failed to remove directory: {}", item.display())
+                        })?;
+                    } else {
+                        fs::remove_file(item).with_context(|| {
+                            format!("Failed to remove file: {}", item.display())
+                        })?;
+                    }
+
+                    if i % 25 == 0 || i + 1 == items.len() {
+                        pbr.set_position((i + 1).try_into().unwrap());
+                    }
+                }
+
+                pbr.finish();
+            }
+
+            info!("Trash was successfully emptied.");
+        }
+
+        Action::TrashPath => {
+            let current_dir = std::env::current_dir()
+                .context("Failed to determine path to the current directory")?;
+
+            let trash_dir = determine_trash_dir_for(&current_dir, &exclude_dirs)?;
+
+            println!("{}", trash_dir.display());
+        }
     }
 
     Ok(())
